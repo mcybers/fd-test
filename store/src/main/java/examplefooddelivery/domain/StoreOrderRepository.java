590536096478b3adb01bc66b20c3a08f109cea9b
mcybers <mcyber@naver.com>
2022-11-21 04:51:34 +0000

    readme

--- store/src/main/java/examplefooddelivery/domain/StoreOrderRepository.java
+++ store/src/main/java/examplefooddelivery/domain/StoreOrderRepository.java
@@ -1,10 +1,14 @@
 package examplefooddelivery.domain;
 
 import examplefooddelivery.domain.*;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel="storeOrders", path="storeOrders")
 public interface StoreOrderRepository extends PagingAndSortingRepository<StoreOrder, Long>{
 
+    Optional<StoreOrder> findByOrderId(Long orderId);
 }
